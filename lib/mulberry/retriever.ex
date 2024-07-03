d29c8b1d0ae843e769a77f66ac331b4461472b82
Thomas Brewer <tom.brewer@mac.com>
2024-07-03 09:54:33 -0400

    chore: adding some logging

--- lib/mulberry/retriever.ex
+++ lib/mulberry/retriever.ex
@@ -1,8 +1,11 @@
 defmodule Mulberry.Retriever do
+  require Logger
+
   @callback get(String.t(), Keyword.t()) ::
               {:ok, map()} | {:error, atom()}
 
   def get(module, url, opts \\ []) do
+    Logger.debug("#{__MODULE__}.get/2 module=#{inspect(module)} url=#{inspect(url)} opts=#{inspect(opts)}")
     module.get(url, opts)
   end
 end
